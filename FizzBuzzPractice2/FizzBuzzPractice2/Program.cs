1ac68049a375511e402a6e97f52d6ac19c44cbed
ABardot <aabardot5@gmail.com>
2018-05-14 19:47:10 -0400

    First commit.

---
+++ FizzBuzzPractice2/FizzBuzzPractice2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3 Fizz
+            // 5 Buzz
+            // 15 FizzBuzz
+
+        }
+    }
+}
