24b2ca8234220147148f400e86bd162adc52f443
ABardot <aabardot5@gmail.com>
2018-05-14 20:17:36 -0400

    Refactored FizzBuzz divisible by 3,5 & 15

--- FizzBuzzPractice2/FizzBuzzPractice2/Program.cs
+++ FizzBuzzPractice2/FizzBuzzPractice2/Program.cs
@@ -14,15 +14,27 @@ namespace FizzBuzzPractice2
             // 5 Buzz
             // 15 FizzBuzz
 
+            //Print number from 1 to 100
+            for (int i = 1; i < 100; i++)
+            {
+                string result = "";
+                // Divisible by 3 print Fizz
+                if (i % 3 == 0) result = "Fizz";
+
+                // Divisible by 5 print Buzz
+                if (i % 5 == 0) result = "Buzz";
+
+                // Divisible by 3 and 5 print FizzBuzz
+                if (i % 15 == 0) result = "FizzBuzz";
+
+                // Print if not divisible by 3 or 5
+                if (result.Length == 0) result = i.ToString();
 
 
+                // Print Number 
+                Console.WriteLine(result);
 
-
-
-
-
-
-
+            }
 
             //for (int i = 1; i < 100; i++)
             //{
